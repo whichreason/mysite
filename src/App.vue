bb04606991823953fb2a16d3d18634bca42b5ac3
whichreason <610986806@qq.com>
2023-12-21 23:48:16 +0800

    “firstone”

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<div id='app'>
+    <loginOne></loginOne>
+</div>
+</template>
+
+<script>
+import loginOne from './views/login-one.vue'
+
+export default {
+  components: {
+    loginOne
+  }
+}
+</script>
+
+<style>
+
+</style>
